463e2e251ce1221eb1b03ba21a2ba416d9a492af
arthurbfonseca27 <arthurbfonseca27@gmail.com>
2024-07-08 01:02:17 -0300

    Feat: Criacao da tela de erro

---
+++ src/routes/Error404.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Logo from "../assets/Erro.svg";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex items-center justify-center">
+        <img src={Logo} alt="Logo da plataforma"></img>
+      </div>
+      <div className="flex font-light flex-row gap-8 justify-center items-center pt-8">
+        <Button size="lg" colorScheme="primary" fontWeight="normal">
+          <nav>
+            <Link to="/">Voltar para Home</Link>
+          </nav>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
